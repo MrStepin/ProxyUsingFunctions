354334874dd342c252e9a15d50bf19c421f7b1fb
Стёпин Станислав Александрович <stanislavalex@mail.ru>
2021-08-19 23:02:34 +0300

    .

--- ProxyUsingFunctions/Program.cs
+++ ProxyUsingFunctions/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,38 @@ namespace ProxyUsingFunctions
     {
         static void Main(string[] args)
         {
+            Action print = FileRead;
+
+            string[] Users = { "Ivan", "Sergey","Vladimir", "Pavel", "Andrey" };
+
+            Console.WriteLine("Enter you name:");
+
+            string userName = Console.ReadLine();
+
+            Predicate<string> nameExist = delegate (string name) { return name == userName; };
+
+            foreach (string name in Users)
+                if(nameExist(name))
+                {
+                    ReadFile();
+                    print();
+                }
+
+            Console.ReadLine();
         }
+
+        public static void ReadFile()
+        {
+            StreamReader file = new StreamReader("C:\\Users\\Stas\\Desktop\\config.txt");
+            string line = file.ReadLine();
+         
+        }
+
+        public static void FileRead()
+        {
+            Console.WriteLine("File read!");
+        }
+
+        
     }
 }
